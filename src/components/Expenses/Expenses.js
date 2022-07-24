cf66e301a33ad0b2be82d9e75ffd7a1f8bd11060
Kemari <kemaric@gmail.com>
2022-07-24 07:11:56 -0400

    adds expense filter to expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,30 @@
+import React from "react";
+import { useState } from "react";
 import "./Expenses.css"
 import ExpenseItem from "./ExpenseItem";
 import { Card } from "../UI/Card";
+import { ExpenseFilter } from "../ExpenseFilter/ExpenseFilter";
+
 export function Expenses(props) {
     const { list } = props;
+    const [filteredList = [], setFilteredList] = useState(list);
+    const [selectedFilter, setFilter] = useState('');
+    // const [yearFilters, setYearFilters] = useState(null);
+
+    const onUpdateFilter = (e) => {
+        const newFilter = parseInt(e);
+        setFilter(e => { 
+            return newFilter; 
+        });
+        setFilteredList((previous = []) => {
+            return previous.filter(item => item.date.getFullYear() === newFilter)
+        });
+    }
     return (<Card className="expenses">
-        {list.map(expense => (
+        <div>
+            <ExpenseFilter yearFilter={selectedFilter} onFilterChange={onUpdateFilter}/>
+        </div>
+        {filteredList.map(expense => (
             <ExpenseItem
             key={expense.id}
             date={expense.date}
@@ -14,4 +34,3 @@ export function Expenses(props) {
     </Card>);
 }
 
-// export default Expenses;
